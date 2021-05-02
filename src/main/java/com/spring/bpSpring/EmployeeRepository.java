1182badc29fdad8d59e50bef024c0d06b4d98c26
Robbe <robbe.dewintere@student.hogent.be>
2021-05-02 12:31:47 +0200

    Fix problem entityManagerFactory

--- src/main/java/com/spring/bpSpring/EmployeeRepository.java
+++ src/main/java/com/spring/bpSpring/EmployeeRepository.java
@@ -3,6 +3,9 @@ package com.spring.bpSpring;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
+@Transactional
 public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
 }
